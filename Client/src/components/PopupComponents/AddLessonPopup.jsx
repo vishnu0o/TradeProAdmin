eb6168fbe930305ff4362b3623ab650680d1a857
lilacVishnu <vishnu.v@lilacinfotech.com>
2024-09-21 22:27:38 +0530

    Referal level

--- Client/src/components/PopupComponents/AddLessonPopup.jsx
+++ Client/src/components/PopupComponents/AddLessonPopup.jsx
@@ -16,115 +16,148 @@ import { useDispatch } from "react-redux";
 import { courseLessonCreateAction } from "../../Redux/Action/courseAction";
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-    "& .MuiDialogContent-root": {
-        padding: theme.spacing(2),
-    },
-    "& .MuiDialogActions-root": {
-        padding: theme.spacing(1),
-    },
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2)
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1)
+  }
 }));
 
-export default function AddLessonPopup({ openAddLesson, setOpenAddLesson }) {
-    const location = useLocation();
-    const query = new URLSearchParams(location.search);
-  
-    // Destructure query parameters
-    const id = query.get("id");
-    const dispatch = useDispatch()
-    const [lessonTitle, setLessonTitle] = useState("");
-    const [error, setError] = useState({});
+export default function AddLessonPopup({
+  openAddLesson,
+  setOpenAddLesson,
+  lessonHeading,
+  lessonSubHeading,
+  placeHolder
+}) {
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
 
-    const handleClose = () => {
-        setOpenAddLesson(false);
-    };
+  // Destructure query parameters
+  const id = query.get("id");
+  const dispatch = useDispatch();
+  const [lessonTitle, setLessonTitle] = useState("");
+  const [level, setLevel] = useState("");
 
-    const validate = () => {
-        let errors = {};
+  const [error, setError] = useState({});
 
-        if (lessonTitle === "") {
-            errors.lessonTitle = "Title is required";
-        }
+  const handleClose = () => {
+    setOpenAddLesson(false);
+  };
 
-        console.log(errors, "errorserrorserrors");
-        setError(errors);
-        return Object.keys(errors).length === 0;
-    };
-    const handleSubmit = () => {
-        if (validate()) {
-            dispatch(courseLessonCreateAction(lessonTitle,id))
-            handleClose()
-        }
-    };
+  const validate = () => {
+    let errors = {};
 
-    return (
-        <>
-            <BootstrapDialog
-                PaperProps={{
-                    style: {
-                        color: "#fff",
-                        maxHeight: "90vh",
-                        display: "flex",
-                        flexDirection: "column",
-                        minWidth: "820px",
-                        backgroundColor: "black",
-                    },
-                }}
-                onClose={handleClose}
-                aria-labelledby="customized-dialog-title"
-                open={openAddLesson}
+    if (lessonTitle === "") {
+      errors.lessonTitle = "Title is required";
+    }
+
+    console.log(errors, "errorserrorserrors");
+    setError(errors);
+    return Object.keys(errors).length === 0;
+  };
+  const handleSubmit = () => {
+    if (validate()) {
+      dispatch(courseLessonCreateAction(lessonTitle, id));
+      handleClose();
+    }
+  };
+
+  return (
+    <>
+      <BootstrapDialog
+        PaperProps={{
+          style: {
+            color: "#fff",
+            maxHeight: "90vh",
+            display: "flex",
+            flexDirection: "column",
+            minWidth: "820px",
+            backgroundColor: "black"
+          }
+        }}
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={openAddLesson}
+      >
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          {lessonHeading ? lessonHeading : "Lesson"}
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={(theme) => ({
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: theme.palette.grey[600],
+            "&:hover": {
+              color: "#fff"
+            }
+          })}
+        >
+          <CloseIcon />
+        </IconButton>
+        <Divider
+          sx={{
+            borderColor: "#eee",
+            borderWidth: ".1px",
+            opacity: 0.2,
+            ml: 2,
+            mr: 2,
+            mb: 1
+          }}
+        />
+        <DialogContent dividers>
+          <Box sx={{ mt: 1 }}>
+            <Typography
+              sx={{
+                mb: 1,
+                color: "#F1F1F1",
+                fontWeight: 500,
+                fontSize: "14px"
+              }}
             >
-                <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-                    Lesson
-                </DialogTitle>
-                <IconButton
-                    aria-label="close"
-                    onClick={handleClose}
-                    sx={(theme) => ({
-                        position: "absolute",
-                        right: 8,
-                        top: 8,
-                        color: theme.palette.grey[600],
-                        "&:hover": {
-                            color: "#fff",
-                        },
-                    })}
-                >
-                    <CloseIcon />
-                </IconButton>
-                <Divider
-                    sx={{
-                        borderColor: "#eee",
-                        borderWidth: ".1px",
-                        opacity: 0.2,
-                        ml: 2,
-                        mr: 2,
-                        mb: 1,
-                    }}
-                />
-                <DialogContent dividers>
-                    <Box sx={{ mt: 1 }}>
-                        <Typography
-                            sx={{
-                                mb: 1,
-                                color: "#F1F1F1",
-                                fontWeight: 500,
-                                fontSize: "14px",
-                            }}
-                        >
-                            Lesson Title
-                        </Typography>
-                        <InputField label="Lesson title" handleChange={(e) => setLessonTitle(e.target.value)} value={lessonTitle} />
-                    </Box>
-                    <Typography sx={{ color: "red" }}>{error?.lessonTitle}</Typography>
-                </DialogContent>
-                <DialogActions
-                    style={{
-                        padding: "0 17px 30px ",
-                    }}
-                >
-                    <SubmitButton title={"Save"} component="addCourse" widthSize="100%" bgColor="#6255FA" borderRadius="4px" heightSize="52px" type="click" handleSubmit={handleSubmit} />
-                </DialogActions>
-            </BootstrapDialog>
-        </>
-    );
+              {lessonSubHeading ? lessonSubHeading : "Lesson Title"}
+            </Typography>
+            {lessonHeading == "Levels" && (
+                <Box sx={{mb:1}}>
+
+
+                    <InputField
+                      label={"Level"}
+                      handleChange={(e) => setLevel(e.target.value)}
+                      value={level}
+                    />
+
+                </Box>
+            )}
+            <InputField
+              label={placeHolder?placeHolder:"Lesson title"}
+              handleChange={(e) => setLessonTitle(e.target.value)}
+              value={lessonTitle}
+            />
+          </Box>
+          <Typography sx={{ color: "red" }}>{error?.lessonTitle}</Typography>
+        </DialogContent>
+        <DialogActions
+          style={{
+            padding: "0 17px 30px "
+          }}
+        >
+          <SubmitButton
+            title={"Save"}
+            component="addCourse"
+            widthSize="100%"
+            bgColor="#6255FA"
+            borderRadius="4px"
+            heightSize="52px"
+            type="click"
+            handleSubmit={handleSubmit}
+          />
+        </DialogActions>
+      </BootstrapDialog>
+    </>
+  );
 }
--- Client/src/components/RefferalManagement/ReferralsManagement.jsx
+++ Client/src/components/RefferalManagement/ReferralsManagement.jsx
@@ -1,9 +1,201 @@
-import React from 'react'
+import { Box, Button, Typography } from "@mui/material";
+import React, { useState } from "react";
+import ClearAllIcon from "@mui/icons-material/ClearAll";
+import { CiEdit } from "react-icons/ci";
+import AddLessonPopup from "../PopupComponents/AddLessonPopup";
 
 const ReferralsManagement = () => {
-  return (
-    <div>ReferralsManagement</div>
-  )
-}
+  const [openAddLesson, setOpenAddLesson] = useState(false);
 
-export default ReferralsManagement
+  return (
+    <>
+      <Typography sx={{ fontWeight: 800, fontSize: "19px" }}>
+        Referral Program Setup:
+      </Typography>
+
+      <Box sx={{ display: "flex" }}>
+        <Typography
+          sx={{ fontSize: "14px", fontWeight: "700", mt: 2, mb: 0.8 }}
+        >
+          Levels
+        </Typography>
+        <Button
+          onClick={() => setOpenAddLesson(true)}
+          sx={{
+            display: "inline-flex",
+            background: "#231F20",
+            textTransform: "capitalize",
+            ml: "auto",
+            mb: 1,
+            "&:hover": {
+              backgroundColor: "#231F20" // Maintain the same background color on hover
+            }
+          }}
+          variant="contained"
+        >
+          <ClearAllIcon sx={{ mr: 1 }} />
+          Add Levels
+        </Button>
+      </Box>
+
+      <Box
+        sx={{
+          border: "1px solid #404F65",
+          borderRadius: "4px",
+          padding: "8px",
+          mb: 1
+        }}
+      >
+        <Box sx={{ display: "flex" }}>
+          <Typography
+            sx={{
+              fontSize: "16px",
+              fontWeight: "500",
+              color: "#404F65",
+              mt: 2,
+              mb: 0.8
+            }}
+          >
+            Level 1
+          </Typography>
+          <Box sx={{ display: "flex", ml: "auto", mt: 1.2 }}>
+            <Typography
+              sx={{ mr: 1, fontSize: "16px", fontWeight: 500, color: "black" }}
+            >
+              Edit
+            </Typography>
+            <CiEdit size={24} />
+          </Box>
+        </Box>
+        <Typography
+          // onClick={() => setOpenAddLesson(true)}
+          sx={{
+            display: "inline-flex",
+            background: "#231F20",
+            textTransform: "capitalize",
+            color: "white",
+            borderRadius: "5px",
+            p: 1,
+            ml: "auto",
+            mb: 1,
+            "&:hover": {
+              backgroundColor: "#231F20" // Maintain the same background color on hover
+            }
+          }}
+          variant="contained"
+        >
+          30% Commission
+        </Typography>
+      </Box>
+
+      <Box
+        sx={{
+          border: "1px solid #404F65",
+          borderRadius: "4px",
+          padding: "8px",
+          mb: 1
+        }}
+      >
+        <Box sx={{ display: "flex" }}>
+          <Typography
+            sx={{
+              fontSize: "16px",
+              fontWeight: "500",
+              color: "#404F65",
+              mt: 2,
+              mb: 0.8
+            }}
+          >
+            Level 1
+          </Typography>
+          <Box sx={{ display: "flex", ml: "auto", mt: 1.2 }}>
+            <Typography
+              sx={{ mr: 1, fontSize: "16px", fontWeight: 500, color: "black" }}
+            >
+              Edit
+            </Typography>
+            <CiEdit size={24} />
+          </Box>
+        </Box>
+        <Typography
+          // onClick={() => setOpenAddLesson(true)}
+          sx={{
+            display: "inline-flex",
+            background: "#231F20",
+            textTransform: "capitalize",
+            color: "white",
+            borderRadius: "5px",
+            p: 1,
+            ml: "auto",
+            mb: 1,
+            "&:hover": {
+              backgroundColor: "#231F20" // Maintain the same background color on hover
+            }
+          }}
+          variant="contained"
+        >
+          30% Commission
+        </Typography>
+      </Box>
+
+      <Typography sx={{ fontSize: "14px", fontWeight: "700", mt: 2, mb: 0.8 }}>
+        Eligibility Criteria:
+      </Typography>
+
+      <Box
+        sx={{
+          border: "1px solid #D5DAE1",
+          borderRadius: "4px",
+          padding: "8px",
+          mb: 1
+        }}
+      >
+        <Typography
+          sx={{ fontWeight: 400, fontSize: "16px", color: "#404F65",pl:1 }}
+        >
+          Send an invite to a friend
+        </Typography>
+      </Box>
+
+      <Box
+        sx={{
+          border: "1px solid #D5DAE1",
+          borderRadius: "4px",
+          padding: "8px",
+          mb: 1
+        }}
+      >
+        <Typography
+          sx={{ fontWeight: 400, fontSize: "16px", color: "#404F65",pl:1 }}
+        >
+          Your friend sign up.
+        </Typography>
+      </Box>
+
+      <Box
+        sx={{
+          border: "1px solid #D5DAE1",
+          borderRadius: "4px",
+          padding: "8px",
+          mb: 1
+        }}
+      >
+        <Typography
+          sx={{ fontWeight: 400, fontSize: "16px", color: "#404F65",pl:1 }}
+        >
+          You’ll both get cash when your friend purchase course.
+        </Typography>
+      </Box>
+
+      <AddLessonPopup
+        openAddLesson={openAddLesson}
+        setOpenAddLesson={setOpenAddLesson}
+        lessonHeading={"Levels"}
+        lessonSubHeading={"Add Level"}
+        placeHolder={"Add Level Commision"}
+      />
+    </>
+  );
+};
+
+export default ReferralsManagement;
